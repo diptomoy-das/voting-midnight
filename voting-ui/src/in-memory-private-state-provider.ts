8e940886599aa465899f15bfbe5d2eae152d91c2
diptomoy-das <diptomoydad@gmail.com>
2026-07-28 14:42:56 +0530

    refactor: complete remaining renaming from bboard to voting across api and contract tests

--- voting-ui/src/in-memory-private-state-provider.ts
+++ voting-ui/src/in-memory-private-state-provider.ts
@@ -1,5 +1,5 @@
 /*
- * This file is part of example-bboard.
+ * This file is part of voting-dapp.
  * Copyright (C) Midnight Foundation
  * SPDX-License-Identifier: Apache-2.0
  * Licensed under the Apache License, Version 2.0 (the "License");
